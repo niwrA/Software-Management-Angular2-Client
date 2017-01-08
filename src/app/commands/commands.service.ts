621ccff115234585768421b592e52d5e4f69a62f
niwrA <arwin@niwra.nl>
2017-01-08 19:54:56 +0100

    WIP

--- src/app/commands/commands.service.ts
+++ src/app/commands/commands.service.ts
@@ -6,7 +6,7 @@ export class CommandsService {
 
   constructor() { }
 
-  createCommand(name: string, entityGuid: string, entityName: string): Command {
+  createCommand(name: string, entityName: string, entityGuid: string): Command {
     let command = new Command(name, entityName, entityGuid);
     command.CreateDate = new Date(); // todo: get UTC date, must be comparable globally to determine order of creation
     command.EntityGuid = entityGuid;
--- src/app/projects/project/project.commands.ts
+++ src/app/projects/project/project.commands.ts
@@ -1,4 +1,7 @@
 import { Command } from '../../commands/command';
+import { Project } from '../project';
 export class ProjectCommand extends Command {
-    constructor(name: string) { super(name, 'Project'); };
+    constructor(project: Project) { 
+        super(project.Name, 'Project', project.Guid); 
+    };
 }
