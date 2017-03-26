79328e83549d7117f9d01080cc2bc53fb4934e2a
niwrA <arwin@niwra.nl>
2017-03-26 01:00:28 +0100

    Prevent duplicate productversions in client

--- src/app/products/productversions/productversions.component.ts
+++ src/app/products/productversions/productversions.component.ts
@@ -65,8 +65,7 @@ export class ProductVersionsComponent implements OnInit {
 
   createProductVersion(name: string): void {
     const productversion = this.service.createProductVersion(true, this.product, name);
-    this.productversions.splice(0, 0, productversion);
-    this.product.versions.splice(0, 0, productversion);
+    //this.product.versions.splice(0, 0, productversion);
   }
 
 }
