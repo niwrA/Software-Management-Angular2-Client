3c3e86d557d9f23ae046f26cd0d3a014ffe5a521
niwrA <arwin@niwra.nl>
2017-09-22 12:23:11 +0200

    update environment (production)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  contactsUrl: 'http://localhost:50274/api/contacts',
+  companiesUrl: 'http://localhost:50274/api/companies',
+  linksUrl: 'http://localhost:50274/api/links',
+  filesUrl: 'http://localhost:50274/api/files',
+  projectsUrl: 'http://localhost:50274/api/projects',
+  productsUrl: 'http://localhost:50274/api/products',
+  designsUrl: 'http://localhost:50274/api/designs',
+  employmentsUrl: 'http://localhost:50274/api/employments',
+  projectroleassignmentsUrl: 'http://localhost:50274/api/projectroleassignments',
+  commandsUrl: 'http://localhost:50274/api/commands/batch', // change to eventsource if you want to try posting to the eventsource backend
+  fileUploadUrl: 'http://localhost:50274/api/fileupload',
+  staticFilesUrl: 'http://localhost:50274/uploads'
 };
