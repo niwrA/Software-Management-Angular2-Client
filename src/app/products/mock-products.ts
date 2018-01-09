601ead264dca93beaa28f3be2751cc4f1a9217b0
Arwin van Arum <arwin.vanarum@cgm.com>
2018-01-09 22:52:43 +0100

    Fix mock products
    As a result of configOptions having been renamed on ProductState

--- src/app/products/mock-products.ts
+++ src/app/products/mock-products.ts
@@ -6,34 +6,34 @@ import { ProductConfigOption } from './productconfigoptions/productconfigoption'
 export const PRODUCTS: ProductState[] = [
   {guid: 'product11', name: 'Nice Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product12', name: 'Narco Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product13', name: 'Bombasto Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product14', name: 'Celeritas Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product15', name: 'Magneta Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product16', name: 'RubberMan Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product17', name: 'Dynama Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product18', name: 'Dr IQ Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product19', name: 'Magma Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()},
+  , configOptions: new Array<ProductConfigOption>()},
   {guid: 'product20', name: 'Tornado Suite', description: 'description', businessCase: 'businesscase',
   versions: new Array<ProductVersion>(), features: new Array<ProductFeature>(), issues: new Array<ProductIssue>()
-  , configoptions: new Array<ProductConfigOption>()}
+  , configOptions: new Array<ProductConfigOption>()}
 ];
 
 export class ProductsServiceStub {
